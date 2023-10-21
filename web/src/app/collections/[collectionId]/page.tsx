1068fe6fdfad7968a72aa22d592d1dcc51aa984e
mhshifat <mehedihassanshifat01@gmail.com>
2023-10-21 19:50:17 +0600

    product card and list added

--- web/src/app/collections/[collectionId]/page.tsx
+++ web/src/app/collections/[collectionId]/page.tsx
@@ -1,7 +1,9 @@
+import CollectionProducts from "./collections-products";
+
 export default function CollectionPage() {
   return (
-    <div>
-      <p>Collection Page</p>
+    <div className="mx-auto px-[30px] md:px-[8%] lg:px-[15%] xl:px-[15%] 2xl:px-[2%] 2xl:w-[1438px]">
+      <CollectionProducts />
     </div>
   )
 }

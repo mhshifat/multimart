d45c709676baa63ffd456efdf4542714071a09c5
mhshifat <mehedihassanshifat01@gmail.com>
2023-10-21 22:15:02 +0600

    added container

--- web/src/app/collections/[collectionId]/page.tsx
+++ web/src/app/collections/[collectionId]/page.tsx
@@ -1,9 +1,12 @@
+import { Container } from "@/components/shared";
 import CollectionProducts from "./collections-products";
 
 export default function CollectionPage() {
   return (
-    <div className="mx-auto px-[30px] md:px-[8%] lg:px-[15%] xl:px-[15%] 2xl:px-[2%] 2xl:w-[1438px]">
-      <CollectionProducts />
+    <div className="w-full">
+      <Container>
+        <CollectionProducts />
+      </Container>
     </div>
   )
 }
